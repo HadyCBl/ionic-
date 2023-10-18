a323565ee8a5fd5be4575c9ae05ce985e935b833
Harvey <hadycbl@gmail.com>
2023-10-18 11:22:11 -0600

    conn realizada

--- casi/src/app/home/home.page.ts
+++ casi/src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { jsPDF } from 'jspdf';
+
+
 
 @Component({
   selector: 'app-home',
@@ -8,18 +11,34 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 users :any =[];
+
+
+formattedData: string = '';
   constructor(private http: HttpClient ) {}
 
   getUser(){
-    this.http.get('http://localhost/app/user.php').subscribe(
+    this.http.get('https://demo.sotecprotech.com/views/aho/prueba3.php').subscribe(
       (Response) => {
         console.log(Response);
         this.users = Response;
       },
       (error) => {
-        console.error('Error al realizar la solicitud:', error);
+        console.error('Error en la solicitud HTTP:', error);
       }
     );
 
 }
+generatePDF() {
+  const doc = new jsPDF();
+  let y = 10;
+
+  this.users.forEach((user: any, index: number) => {  // Define el tipo de 'user' y 'index'
+    const shortName = user.short_name;
+    doc.text(`Usuario ${index + 1}: ${shortName}`, 10, y);
+    y += 10;
+  });
+
+  doc.save('usuarios.pdf');
+}
+
 }
