073c2b873530ec6e85b0e7c21e70c8298eb52159
Harvey <hadycbl@gmail.com>
2023-10-23 14:30:57 -0600

    v2 guarda pdf

--- casi/src/app/home/home.page.ts
+++ casi/src/app/home/home.page.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { jsPDF } from 'jspdf';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,11 +9,8 @@ import { jsPDF } from 'jspdf';
 })
 export class HomePage {
 users :any =[];
-
-
 formattedData: string = '';
   constructor(private http: HttpClient ) {}
-
   getUser(){
     this.http.get('https://demo.sotecprotech.com/views/aho/prueba3.php').subscribe(
       (Response) => {
@@ -26,19 +21,25 @@ formattedData: string = '';
         console.error('Error en la solicitud HTTP:', error);
       }
     );
-
 }
 generatePDF() {
   const doc = new jsPDF();
   let y = 10;
 
-  this.users.forEach((user: any, index: number) => {  // Define el tipo de 'user' y 'index'
+  this.users.forEach((user: any, index: number) => {  
     const shortName = user.short_name;
     doc.text(`Usuario ${index + 1}: ${shortName}`, 10, y);
     y += 10;
   });
 
-  doc.save('usuarios.pdf');
+  // Genera el PDF en un Blob
+  const pdfBlob = doc.output('blob');
+
+  //  URL de objeto para el Blob
+  const pdfUrl = URL.createObjectURL(pdfBlob);
+
+  window.open(pdfUrl);
 }
+
 
 }
